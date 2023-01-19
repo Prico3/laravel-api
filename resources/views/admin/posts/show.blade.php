8805a4d7d1100fc4365c303cca0bc9a7008050bf
Prico3 <pricoco.andrea@gmail.com>
2023-01-19 15:52:51 +0100

    first commit

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h1>{{ $post->title }}</h1>
+        <h5>{{ $post->category?->name }}</h5>
+        @if ($post->cover_image)
+            <img src="{{ asset('storage/' . $post->cover_image) }}" alt="{{ 'Cover image di ' . $post->title }}">
+        @else
+            <div class="w-50 bg-secondary py-4 text-center">
+                No image yet
+            </div>
+        @endif
+
+        <p>{{ $post->content }}</p>
+
+        <div class="tags mt-5">
+            @forelse ($post->tags as $tag)
+                <span>{{ $tag->name }}</span>
+
+            @empty
+                <span>Nessuna Tecnologia</span>
+            @endforelse
+        </div>
+
+
+    </div>
+@endsection
